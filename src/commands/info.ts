dca454063fc7d2a179efb3526dd6654cef6f7606
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-01-27 11:37:31 -0600

    Editing help and info embeds

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,7 +5,7 @@ export const name = 'info';
 export async function execute(interaction: CommandInteraction) {
     const infoEmbed = new MessageEmbed()
         .setTitle('What am I?')
-        .setDescription(`I'm a free history bot built for communities to share their love for history, by learning and discussing together. All the events that are displayed are randomly selected from [Wikipedia](https://wikipedia.org/) using a custom API. If you are interested in supporting us, checkout our page on [Patreon](https://www.patreon.com/historicaleventsbot). All donations go directly towards the continued development and maintenance of the bot. \n\nIf you would like to suggest something, report a bug, need help, or want to keep up to date with the bot please join the [Historian Bot Support Server](${config.server_invite})`)
+        .setDescription(`I'm a free history bot built for communities to share their love for history, by learning and discussing together. All the events that are displayed are gathered from [Wikipedia](https://wikipedia.org/). If you are interested in supporting us, checkout our page on [Patreon](https://www.patreon.com/historicaleventsbot). All donations go directly towards the continued development and maintenance of the bot. \n\nIf you would like to suggest something, report a bug, need help, or want to keep up to date with the bot please join the [Historian Bot Support Server](${config.server_invite})`)
         .setColor(config.default_hex as ColorResolvable)
         .setFooter(config.default_footer);
     interaction.reply({ embeds: [infoEmbed], ephemeral: true });
